65cdc66b1f0ed66585af1de8db6e346bde542e26
Kasper Munch <kaspermunch@birc.au.dk>
2025-04-07 14:21:07 +0200

    small update

--- release-tag.sh
+++ release-tag.sh
@@ -3,5 +3,6 @@
 if [[ `git status --porcelain` ]]; then
   echo "Changes to pyproject.toml must be pushed first."
 else
-    git tag -a "v$(python setup.py --version)" -m "${1:-Release}" && git push origin --tags
+    tag="v$(python setup.py --version)"
+    git tag -a $tag -m "${1:-Release}" && git push origin --tags
 fi
